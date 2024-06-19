d0e19df2c7a00f9e294c5488b18608646d671049
fabiano-franca <fabianofranca.ti@gmail.com>
2024-06-19 09:13:32 -0300

    Feat: Interface finalizada

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,10 +1,11 @@
+import { useNavigation } from "@react-navigation/native";
 import { Center, Image, VStack, Text, Heading, ScrollView } from "native-base";
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
 
 export function SignUp(){
 
